11f3984585e150eb3724d4011914607ddb212241
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-09 08:24:37 -0700

    api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,45 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
 
 
 function Home() {
+  const [products, setProducts] = useState(null)
+
+  useEffect(()=>{
+    axios('https://fakestoreapi.com/products')
+    .then((res)=>{
+      console.log(res.data);
+      setProducts(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[])
   return (
     <>
-    
+    <div>
+      {
+        products ? products.map((item)=>{
+          return (
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection : 'column',
+              alignItems: 'center',
+              margin: '20px',
+              border: '1px solid black',
+              padding: '10px',
+              flex: 'wrap'
+            }} key={item.id}>
+              <h3>{item.title}</h3>
+              <img width= '100px' src={item.image} alt={item.title} />
+              <p>${item.price}</p>
+            </div>
+          )
+        }): <h1>Loading...</h1>
+      }
+    </div>
     </>
   )
 }
