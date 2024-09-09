50aed2b56e31b480a1970157ddbc7555038cb537
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-09 07:39:09 -0700

    working on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,33 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Service from './pages/Service.jsx'
 const routes = createBrowserRouter([
   {
     pat : '/',
-    element : <
+    element : <Layout />,
+    children : [
+      {
+        path : '',
+        element : <Home />
+      },
+      {
+        path : 'about',
+        element : <About />
+      },
+      {
+        path : 'contact',
+        element : <Contact /> 
+      },
+      {
+        path : 'Service',
+        element : <Service/>
+      }
+    ]
   }
 ])
 
