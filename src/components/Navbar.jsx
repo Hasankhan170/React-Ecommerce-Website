5cc56d09ac19c517e7d76f841424fe6e433b52bb
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-09 08:00:46 -0700

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+
+function Navbar() {
+  return (
+    <>
+    <Link to="">Home</Link>
+    <Link to="About">About</Link>
+    <Link to="Contact">Contact</Link>
+    <Link to="Service">Service</Link>
+    </>
+  )
+}
+
+export default Navbar
